1dc80a2a0052338745a77b99dd1af4110d3105b0
AaronGabriel147 <77466761+AaronGabriel147@users.noreply.github.com>
2021-11-20 09:44:09 -0800

    all setup, now need to seed and connect data

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,23 @@
-// build your `Project` model here
+// @@@@@@@@@@@@@@@@@@@@ models require data sorta like:
+// const db = require("../../data/db-config");
+
+
+
+// @@@@@@@@@@@@@@@@@@@@@ Then basic functions:
+// function getAll() {
+//     return db("users");
+// }
+
+// function findById(id) {
+//     return db("users")
+//         .where({ id })
+//         .first();
+// }
+
+
+
+// @@@@@@@@@@@@@@@@@@@ Then Exports
+// exports = module.exports = {
+//     getAll,
+//     findById
+// }
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,5 +1,15 @@
 const router = require('express').Router();
 
+
+
+// @@@@@@@ Router model imports look like this:
+// // Models
+// const User = require('../../api/users/user-model')
+// // getAll
+
+
+
+
 // connected to server.js
 router.get('/', (req, res) => {
     res.json({
